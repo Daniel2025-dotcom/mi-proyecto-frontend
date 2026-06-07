4a6b6880a631f8998657e452f5089717dca23654
Daniel2025-dotcom <juandanielgerbacio@gmail.com>
2026-06-06 21:54:35 -0300

    solucinado problema de pre-renderizado

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'productos/:categoria',
+    renderMode: RenderMode.Server 
+  },
+  {
+    path: 'admin/productos/:categoria',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
